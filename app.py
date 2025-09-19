19dfd6a54e5a9012bfdd4012653da9f73cd2d352
Yash_Panchal <152013587+Yash-Panchal-094@users.noreply.github.com>
2025-09-19 17:01:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from utils import add, divide
+
+def main():
+    print("Demo App Running...")
+    print("2 + 3 =", add(2, 3))
+    print("10 / 2 =", divide(10, 2))
+
+if __name__ == "__main__":
+    main()
